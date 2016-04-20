5fa4147de3c3e09d35586fde98de18ae8cb7a8bd
Peter Gribanov <wcode404@gmail.com>
2016-04-20 18:57:48 +0300

    add grunt config file

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+    grunt.file.defaultEncoding = 'UTF-8';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            js: {
+                src: [
+                    'src/extend.js',
+                    'src/Controller.js',
+                    'src/Controller/Control.js',
+                    'src/util/FormToken.js',
+                    'src/util/Locker.js'
+                ],
+                dest: 'build/controller.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                compress: {
+                    drop_console: true
+                }
+            },
+            build: {
+                options: {
+                    mangle: false // do not change the names of the functions in the minification
+                },
+                files: {
+                    'build/controller.min.js': 'build/controller.js'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['concat', 'uglify']);
+};
